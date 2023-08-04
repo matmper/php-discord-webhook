3d32865801abb374e253fe94a09fe41ed49f91c7
Matheus Pereira <contato@matheusp.com.br>
2023-08-04 02:49:03 -0300

    test: enum test

---
+++ tests/EnumTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Tests;
+
+use Matmper\Enums\MessageType;
+
+class EnumTest extends TestCase
+{
+    /**
+     * @test
+     * @covers Enums\MessageType
+     * @dataProvider messageTypeEnumProvider
+     */
+    public function test_message_type_enum(string $type)
+    {
+        $type = strtoupper($type);
+        $constant = !empty(constant("\Matmper\Enums\MessageType::{$type}"));
+        $this->assertTrue($constant);
+    }
+
+    /**
+     * Provider
+     *
+     * @return araay
+     */
+    private function messageTypeEnumProvider(): array
+    {
+        return [
+            [MessageType::SUCCESS],
+            [MessageType::WARNING],
+            [MessageType::DANGER],
+            [MessageType::DEFAULT],
+        ];
+    }
+}
